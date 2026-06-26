5f845cf100f310c99e650797ce6b8593a9149f85
ARSELIX <maternex001@gmail.com>
2026-06-26 13:08:54 +0530

    Add files via upload

--- loop-box.py
+++ loop-box.py
@@ -10,35 +10,27 @@ while True:
 
     choice = int(input("Enter your choice: "))
 
-    
     if choice == 1:
         rows = int(input("Enter number of rows: "))
         if rows <= 0:
             print("Invalid rows")
             break
-
         print("Right Angle Triangle")
         for i in range(1, rows + 1):
             for j in range(i):
                 print("*", end=" ")
             print()
 
-    
     elif choice == 2:
         rows = int(input("Enter number of rows: "))
-
         print("Pyramid Pattern")
         for i in range(rows):
-
             for space in range(rows - i - 1):
                 print(" ", end="")
-
             for j in range(2 * i + 1):
                 print("*", end="")
-
             print()
 
-    
     elif choice == 3:
         rows = int(input("Enter number of rows: "))
         print("Left Angle Triangle")
@@ -49,37 +41,26 @@ while True:
                 print("*", end=" ")
             print()
 
-
-    
     elif choice == 4:
-
         start = int(input("Enter start number: "))
         end = int(input("Enter end number: "))
-
         if end < start:
             print("End must be greater than start")
             continue
 
         total = 0
-
         for i in range(start, end + 1):
-
             if i == 0:
                 pass
-
             if i % 2 == 0:
                 print(i, "is Even")
             else:
                 print(i, "is Odd")
-
             total += i
-
         print("Sum =", total)
 
-    
     elif choice == 5:
         print("Thank you")
         break
-
     else:
         print("Invalid choice")
